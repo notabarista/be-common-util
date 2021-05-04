e83fe632eab5bf1c4663a7ecc73813c70a5a76e1
Jozsef Sas <jozsef.l.sas@gmail.com>
2021-05-04 22:24:14 +0300

    add support for custom headers
    Also created a constants class

--- src/main/java/org/notabarista/service/util/IBackendRequestService.java
+++ src/main/java/org/notabarista/service/util/IBackendRequestService.java
@@ -1,30 +1,31 @@
 package org.notabarista.service.util;
 
-import java.net.URI;
-
 import org.notabarista.entity.response.Response;
 import org.notabarista.exception.AbstractNotabaristaException;
 import org.notabarista.service.util.enums.MicroService;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 
+import java.net.URI;
+import java.util.Map;
+
 public interface IBackendRequestService {
 
 	URI getServiceUri(String microServiceName) throws AbstractNotabaristaException;
 
 	<T extends Object> Response<T> executePost(MicroService microService, String uri, Object object,
-			ParameterizedTypeReference<Response<T>> parameterizedTypeReference) throws AbstractNotabaristaException;
+			ParameterizedTypeReference<Response<T>> parameterizedTypeReference, Map<String, String> customHeaders) throws AbstractNotabaristaException;
 
 	<T extends Object> Response<T> executeGet(MicroService microService, String uri, Object object,
-			ParameterizedTypeReference<Response<T>> parameterizedTypeReference) throws AbstractNotabaristaException;
+			ParameterizedTypeReference<Response<T>> parameterizedTypeReference, Map<String, String> customHeaders) throws AbstractNotabaristaException;
 
 	<T extends Object> Response<T> executeRequest(String uri, HttpMethod httpMethod, Object object,
-			ParameterizedTypeReference<Response<T>> parameterizedTypeReference);
+			ParameterizedTypeReference<Response<T>> parameterizedTypeReference, Map<String, String> customHeaders);
 
 	<T extends Object> Response<T> executePut(MicroService microService, String uri, Object object,
-			ParameterizedTypeReference<Response<T>> parameterizedTypeReference) throws AbstractNotabaristaException;
+			ParameterizedTypeReference<Response<T>> parameterizedTypeReference, Map<String, String> customHeaders) throws AbstractNotabaristaException;
 	
 	<T extends Object> Response<T> executePatch(MicroService microService, String uri, Object object,
-			ParameterizedTypeReference<Response<T>> parameterizedTypeReference) throws AbstractNotabaristaException;
+			ParameterizedTypeReference<Response<T>> parameterizedTypeReference, Map<String, String> customHeaders) throws AbstractNotabaristaException;
 
 }
---
+++ src/main/java/org/notabarista/util/NABConstants.java
@@ -0,0 +1,9 @@
+package org.notabarista.util;
+
+public final class NABConstants {
+
+    public static final String UID_HEADER_NAME = "uid";
+
+    private NABConstants() {}
+
+}
